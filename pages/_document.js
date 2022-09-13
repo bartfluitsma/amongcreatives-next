362eedcaca9a1d9e25f3dc45cf564b4e2891b0fc
bart <bartfluitsma@gmail.com>
2022-09-13 20:28:36 +0800

    Updates to the base, fonts

--- pages/_document.js
+++ pages/_document.js
@@ -16,9 +16,8 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /> 
-                    #TODO optimize fonts for loading*/}
                     <link
                         href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300;0,400;0,600;1,300;1,400&family=Forum&display=optional"
                         rel="stylesheet"
