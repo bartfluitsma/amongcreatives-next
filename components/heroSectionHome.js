cd095f004dd63928669944396bd958fc3e01cf4a
bart <bartfluitsma@gmail.com>
2022-11-15 08:41:35 +0800

    change title

--- components/heroSectionHome.js
+++ components/heroSectionHome.js
@@ -1,20 +1,19 @@
-import PrimaryButton from '../components/primaryButton'
+import PrimaryButton from "../components/primaryButton";
 
 const HeroSectionHome = () => {
-    return (
-        <>
-            <div className="bg-blend-overlay bg-[#00000030] md:bg-[#00000015] bg-[url('/homepage/Women-reading-a-book-against-a-white-wall-portrait.jpg')] md:bg-[url('/homepage/Women-reading-a-book-against-a-white-wall.jpg')] min-h-[86vh] w-full bg-cover bg-center bg-no-repeat pt-[40vh] text-center grid place-items-center md:pr-[50vw] md:pt-0">
-                <div className='max-w-xs m-auto md:max-w-lg md:top-[25vh] md:bottom-auto md:left-[10vw]'>
-                    <h1 className="text-white">Fashion &amp; Product photography</h1>
-                    <p className="text-white text-Forum pb-2">Sustainable &amp; ethical Brands</p>
-                    <PrimaryButton link="/book">
-                        Book a shoot
-                    </PrimaryButton>
-                </div>
-            </div>
-
-        </>
-    );
-}
+  return (
+    <>
+      <div className="bg-blend-overlay bg-[#00000030] md:bg-[#00000015] bg-[url('/homepage/Women-reading-a-book-against-a-white-wall-portrait.jpg')] md:bg-[url('/homepage/Women-reading-a-book-against-a-white-wall.jpg')] min-h-[86vh] w-full bg-cover bg-center bg-no-repeat pt-[40vh] text-center grid place-items-center md:pr-[50vw] md:pt-0">
+        <div className="max-w-xs m-auto md:max-w-lg md:top-[25vh] md:bottom-auto md:left-[10vw]">
+          <h1 className="text-white">Fashion &amp; Product photography</h1>
+          <p className="text-white text-Forum pb-2">
+            For Sustainable &amp; Ethical Brands
+          </p>
+          <PrimaryButton link="/book">Book a shoot</PrimaryButton>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HeroSectionHome;
